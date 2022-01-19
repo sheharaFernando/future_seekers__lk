b04e18f9933ecd2fd17a1a7eea5fd3b043deb50d
Manish Nirmal <cb008909@students.apiit.lk>
2022-01-19 23:47:41 +0530

    v1.2
    v1.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,91 @@
 <!-- Inner Page Title start --> 
 
 @include('includes.inner_page_title', ['page_title'=>__('Dashboard')]) 
-
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 <!-- Inner Page Title end -->
-
+<style>
+    .alert{
+  background: #ffdb9b;
+  padding: 20px 40px;
+  min-width: 420px;
+  position: absolute;
+  right: 0;
+  top: 10px;
+  border-radius: 4px;
+  border-left: 8px solid #ffa502;
+  overflow: hidden;
+  opacity: 0;
+  pointer-events: none;
+}
+.alert.showAlert{
+  opacity: 1;
+  pointer-events: auto;
+}
+.alert.show{
+  animation: show_slide 1s ease forwards;
+}
+@keyframes show_slide {
+  0%{
+    transform: translateX(100%);
+  }
+  40%{
+    transform: translateX(-10%);
+  }
+  80%{
+    transform: translateX(0%);
+  }
+  100%{
+    transform: translateX(-10px);
+  }
+}
+.alert.hide{
+  animation: hide_slide 1s ease forwards;
+}
+@keyframes hide_slide {
+  0%{
+    transform: translateX(-10px);
+  }
+  40%{
+    transform: translateX(0%);
+  }
+  80%{
+    transform: translateX(-10%);
+  }
+  100%{
+    transform: translateX(100%);
+  }
+}
+.alert .fa-exclamation-circle{
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #ce8500;
+  font-size: 30px;
+}
+.alert .msg{
+  padding: 0 20px;
+  font-size: 18px;
+  color: #ce8500;
+}
+.alert .close-btn{
+  position: absolute;
+  right: 0px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: #ffd080;
+  padding: 20px 18px;
+  cursor: pointer;
+}
+.alert .close-btn:hover{
+  background: #ffc766;
+}
+.alert .close-btn .fas{
+  color: #ce8500;
+  font-size: 22px;
+  line-height: 40px;
+}
+</style>
 <div class="listpgWraper">
 
     <div class="container">@include('flash::message')
@@ -48,7 +130,7 @@
 
 							</div>
 
-							<div class="col-lg-5"><div class="editbtbn"><a href="{{ route('my.profile') }}"><i class="fas fa-pencil-alt" aria-hidden="true"></i> Edit Profile</a>
+							<div class="col-lg-5"><div class="editbtbn"><a href="{{ route('my.profile') }}"><i class="fas fa-pencil-alt" aria-hidden="true"></i>Edit Profile</a>
 
 						</div></div>
 
@@ -227,8 +309,40 @@
     </div>
 
 </div>
+      <div class="alert hide">
 
+         <span class="msg">Welcome!</span>
+         <div class="close-btn">
+            
+         </div>
+      </div>
 @include('includes.footer')
+
+<script defer="defer" src="https://alertifyjs.com/js/jquery-1.11.1.min.js"></script>
+      <script>
+         $('button').click(function(){
+           $('.alert').addClass("show");
+           $('.alert').removeClass("hide");
+           $('.alert').addClass("showAlert");
+           setTimeout(function(){
+             $('.alert').removeClass("show");
+             $('.alert').addClass("hide");
+           },5000);
+         });
+         $('.close-btn').click(function(){
+           $('.alert').removeClass("show");
+           $('.alert').addClass("hide");
+         });
+         window.onload = function() {
+   $('.alert').addClass("show");
+           $('.alert').removeClass("hide");
+           $('.alert').addClass("showAlert");
+           setTimeout(function(){
+             $('.alert').removeClass("show");
+             $('.alert').addClass("hide");
+           },5000);
+}
+      </script>
 
 @endsection
 
